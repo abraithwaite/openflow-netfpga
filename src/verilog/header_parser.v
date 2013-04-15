bbb058cd5a54e295f39997ecacb64abf972d1907
Alan Braithwaite <abraithwaite@mail.csuchico.edu>
2013-04-14 20:51:31 -0700

    Updated Header Parser

--- src/verilog/header_parser.v
+++ src/verilog/header_parser.v
@@ -11,157 +11,162 @@ module passthrough
       output                                 in_rdy,
 
       // --- Interface to the next stage
-      output reg [DATA_WIDTH-1:0]            out_data,
-      output reg [CTRL_WIDTH-1:0]            out_ctrl,
-      output reg                             out_wr,
-      input                                  out_rdy,
+      output reg [`OF_HEADER_REG_WIDTH-1:0]  header_bus,
+      output reg                             headers_valid;
 
       // --- Misc
       input                                  reset,
       input                                  clk
    );
 
-   localparam  PRE_READ_HDR    = 1,
-               PRE_WAIT_EOP    = 2;
+   localparam  RD_INGRESS_PORT      = 1,
+               RD_DLDST_DLSRCH      = 2,
+               RD_DLSRCL_DLTYPE_TOS = 3,
+               RD_NWPROTO           = 4,
+               RD_NWSRC_NWDSTH      = 5,
+               RD_NWDSTL_TX         = 6,
+               RD_TX_MORE           = 7,
+               WAIT_EOP             = 0;
 
-   localparam  RX_QUEUE_HDR    = 8'hFF;
-
-   localparam  PRO_WAIT        = 1,
-               PRO_MODIFY_HDR  = 2,
-               PRO_WRITE       = 4;
-
-   localparam  PORT0           = 0,
-               PORT1           = 2,
-               PORT2           = 4,
-               PORT3           = 6;
-
-   wire [DATA_WIDTH-1:0] in_fifo_data_out;
-   wire [CTRL_WIDTH-1:0] in_fifo_ctrl_out;
-
-   reg [1:0]             pre_state;
-   reg [`OF_IN_PORT-1:0]            src_port;
-   reg                   port_vld;
-
-   reg [2:0]             pro_state;
-   reg [2:0]             pro_state_nxt;
-   reg                   in_fifo_rd_en;
-   reg                   out_wr_nxt;
-   reg [DATA_WIDTH-1:0]  out_data_nxt;
-   reg [CTRL_WIDTH-1:0]  out_ctrl_nxt;
-
-   fallthrough_small_fifo
-   #(.WIDTH(CTRL_WIDTH+DATA_WIDTH), .MAX_DEPTH_BITS(3))
-   input_fifo(
-   .din           ({in_ctrl, in_data}),  // Data in
-   .wr_en         (in_wr),             // Write enable
-   .rd_en         (in_fifo_rd_en),    // Read the next word
-   .dout          ({in_fifo_ctrl_out, in_fifo_data_out}),
-   .full          (),
-   .prog_full     (),
-   .nearly_full   (in_fifo_nearly_full),
-   .empty         (in_fifo_empty),
-   .reset         (reset),
-   .clk           (clk)
-   );
-
-   // -- Logic -- //
-   assign in_rdy = !in_fifo_nearly_full;
+   reg [3:0]      rd_state;
+   reg            rd_is_ip, rd_is_xdp, rd_is_icmp;
+   reg            rd_is_ipfrag;
+   reg [3:0]      rd_hdr_len;
 
    always @(posedge clk) begin
       if(reset) begin
-         // Default states
-         pre_state   <= PRE_READ_HDR;
-         port_vld    <= 0;
-         src_port    <= 0;
+         rd_state <= RD_INGRESS_PORT;
+         headers_valid <= 0;
+         header_bus <= 0;
       end
       else begin
-         case (pre_state)
-            // Read headers as soon as we have the right one
-            PRE_READ_HDR: begin
-               if (in_wr && in_ctrl == `IO_QUEUE_STAGE_NUM) begin
-                  src_port  <= in_data[31:16];
-                  port_vld <= 1;
-                  pre_state <= PRE_WAIT_EOP;
+         if (in_wr) begin
+            case (rd_state)
+               RD_INGRESS_PORT: begin
+                  if (in_ctrl == `IO_QUEUE_STAGE_NUM) begin
+                     header_bus[`OF_IN_PORT + `OF_IN_PORT_POS:`OF_IN_PORT_POS] <= in_data[31:16];
+                     rd_state <= RD_DLDST_DLSRCH;
+                  end
                end
-            end
-
-            PRE_WAIT_EOP: begin
-               if (in_wr && in_ctrl != 0) begin
-                  port_vld <= 0;
-                  pre_state <= PRE_READ_HDR;
+               RD_DLDST_DLSRCH: begin
+                  if (in_ctrl == 0) begin
+                     header_bus[`OF_DL_DST + `OF_DL_DST_POS:`OF_DL_DST_POS] <= in_data[63:16];
+                     header_bus[`OF_DL_SRC_POS + 15:`OF_DL_SRC_POS] <= in_data[15:0];
+                     rd_state <= RD_DLSRCL_DLTYPE_TOS;
+                  end
                end
-            end
-         endcase
+               RD_DLSRCL_DLTYPE_TOS: begin
+                  if (in_ctrl == 0) begin
+                     header_bus[`OF_DL_SRC + `OF_DL_SRC_POS :`OF_DL_SRC_POS + 16 ] <= in_data[63:32];
+                     header_bus[`OF_DL_TYPE + `OF_DL_TYPE_POS :`OF_DL_TYPE_POS ] <= in_data[31:16];
+                     // If Ethertype is IP
+                     if (in_data[31:16] == 16'h0800) begin
+                        header_bus[`OF_NW_TOS + `OF_NW_TOS_POS :`OF_NW_TOS_POS ] <= in_data[7:0];
+                        rd_hdr_len <= in_data[11:8];
+                        rd_is_ip <= 1;
+                        rd_state <= RD_NWPROTO;
+                     end
+                     else begin
+                        headers_valid <= 1;
+                        rd_state <= WAIT_EOP;
+                     end
+                  end
+               end
+               RD_NWPROTO: begin
+                  if (in_ctrl == 0) begin
+                     header_bus[`OF_NW_PROTO + `OF_NW_PROTO_POS :`OF_NW_PROTO_POS ] <= in_data[7:0];
+                     // IP Fragmentation not supported
+                     if (in_data[15] != in_data[13] or in_data[12:0] != 13'h0000) begin
+                        rd_is_ipfrag <= 1;
+                     end
+                     rd_state <= RD_NWSRC_NWDSTH;
+                  end
+               end
+               RD_NWSRC_NWDSTH: begin
+                  if (in_ctrl == 0) begin
+                     header_bus[`OF_NW_SRC + `OF_NW_SRC_POS :`OF_NW_SRC_POS ] <= in_data[47:16];
+                     header_bus[`OF_NW_DST_POS+ 15:`OF_NW_DST_POS ] <= in_data[15:0];
+                     rd_state <= RD_NWDSTL_TX;
+                  end
+               end
+               RD_NWDSTL_TX: begin
+                  header_bus[`OF_NW_DST + `OF_NW_DST_POS:`OF_NW_DST_POS + 16] <= in_data[63:48];
+                  if (rd_is_ipfrag) begin
+                     headers_valid <= 1;
+                     rd_state <= WAIT_EOP;
+                  end else begin
+                     if (rd_hdr_len < 6) begin
+                        if (rd_is_icmp) begin
+                           header_bus[`OF_TP_SRC + `OF_TP_SRC_POS :`OF_TP_SRC_POS] <= in_data[47:40];
+                           header_bus[`OF_TP_DST + `OF_TP_DST_POS :`OF_TP_DST_POS] <= in_data[39:32];
+                        end else if (rd_is_xdp) begin
+                           header_bus[`OF_TP_SRC + `OF_TP_SRC_POS :`OF_TP_SRC_POS] <= in_data[47:32];
+                           header_bus[`OF_TP_DST + `OF_TP_DST_POS :`OF_TP_DST_POS] <= in_data[31:16];
+                        end
+                        headers_valid <= 1;
+                        if ( in_ctrl != 0 ) begin
+                           rd_state <= RD_INGRESS_PORT;
+                        end else begin
+                           rd_state <= WAIT_EOP;
+                        end
+                     end else if (rd_hdr_len == 6) begin
+                        if (rd_is_icmp) begin
+                           header_bus[`OF_TP_SRC + `OF_TP_SRC_POS :`OF_TP_SRC_POS] <= in_data[15:8];
+                           header_bus[`OF_TP_DST + `OF_TP_DST_POS :`OF_TP_DST_POS] <= in_data[7:0];
+                           headers_valid <= 1;
+                           rd_state <= WAIT_EOP;
+                        end else if (rd_is_xdp) begin
+                           header_bus[`OF_TP_SRC + `OF_TP_SRC_POS :`OF_TP_SRC_POS] <= in_data[15:0];
+                           rd_hdr_len <= rd_hdr_len - 2;
+                           rd_state <= RD_TX_MORE;
+                        end
+                     end else begin // IP Header Len > 6
+                        rd_hdr_len <= rd_hdr_len - 2;
+                        rd_state <= RD_TX_MORE;
+                     end
+                  end
+               end
+               RD_TX_MORE: begin
+                  if (rd_hdr_len < 6) begin
+                     if (rd_is_icmp) begin
+                        header_bus[`OF_TP_SRC + `OF_TP_SRC_POS :`OF_TP_SRC_POS] <= in_data[47:40];
+                        header_bus[`OF_TP_DST + `OF_TP_DST_POS :`OF_TP_DST_POS] <= in_data[39:32];
+                     end else if (rd_is_xdp) begin
+                        header_bus[`OF_TP_SRC + `OF_TP_SRC_POS :`OF_TP_SRC_POS] <= in_data[47:32];
+                        header_bus[`OF_TP_DST + `OF_TP_DST_POS :`OF_TP_DST_POS] <= in_data[31:16];
+                     end
+                     headers_valid <= 1;
+                     if ( in_ctrl != 0 ) begin
+                        rd_state <= RD_INGRESS_PORT;
+                     end else begin
+                        rd_state <= WAIT_EOP;
+                     end
+                  end else if (rd_hdr_len == 6) begin
+                     if (rd_is_icmp) begin
+                        header_bus[`OF_TP_SRC + `OF_TP_SRC_POS :`OF_TP_SRC_POS] <= in_data[15:8];
+                        header_bus[`OF_TP_DST + `OF_TP_DST_POS :`OF_TP_DST_POS] <= in_data[7:0];
+                        headers_valid <= 1;
+                        rd_state <= WAIT_EOP;
+                     end else if (rd_is_xdp) begin
+                        header_bus[`OF_TP_SRC + `OF_TP_SRC_POS :`OF_TP_SRC_POS] <= in_data[15:0];
+                        rd_hdr_len <= rd_hdr_len - 2;
+                        rd_state <= RD_TX_MORE;
+                     end
+                  end else begin // IP Header Len > 6
+                     rd_hdr_len <= rd_hdr_len - 2;
+                     rd_state <= RD_TX_MORE;
+                  end
+               end
+               WAIT_EOP: begin
+                  if (in_ctrl != 0) begin
+                     headers_valid <= 0;
+                     header_bus <= 0;
+                     rd_state <= PRE_READ_HDR;
+                  end
+               end
+            endcase
+         end
       end
    end
-
-   /* This block modifies the port and sends it to the output
-   */
-   always @(*) begin
-      pro_state_nxt       = pro_state;
-      in_fifo_rd_en       = 0;
-      out_wr_nxt          = 0;
-      out_data_nxt        = in_fifo_data_out;
-      out_ctrl_nxt        = in_fifo_ctrl_out;
-      case (pro_state)
-         PRO_WAIT: begin
-            if (port_vld) begin
-               pro_state_nxt = PRO_MODIFY_HDR;
-            end
-         end
-
-         PRO_MODIFY_HDR: begin
-            if (out_rdy && !in_fifo_empty) begin
-               in_fifo_rd_en  = 1;
-               out_wr_nxt  = 1;
-               case(src_port)
-                  PORT0: begin
-                     out_data_nxt[`IOQ_DST_PORT_POS+15:`IOQ_DST_PORT_POS] = 1 << PORT1;
-                  end
-                  PORT1: begin
-                     out_data_nxt[`IOQ_DST_PORT_POS+15:`IOQ_DST_PORT_POS] = 1 << PORT0;
-                  end
-                  PORT2: begin
-                     out_data_nxt[`IOQ_DST_PORT_POS+15:`IOQ_DST_PORT_POS] = 1 << PORT3;
-                  end
-                  PORT3: begin
-                     out_data_nxt[`IOQ_DST_PORT_POS+15:`IOQ_DST_PORT_POS] = 1 << PORT2;
-                  end
-               endcase
-               pro_state_nxt = PRO_WRITE;
-            end
-         end
-
-         PRO_WRITE: begin
-            if (out_rdy && !in_fifo_empty) begin
-               // End of packet data marked by non-zero ctrl
-               if (in_fifo_ctrl_out != 0) begin
-                  pro_state_nxt = PRO_WAIT;
-               end
-               in_fifo_rd_en   = 1;
-               out_wr_nxt      = 1;
-            end
-         end
-      endcase
-   end
-
-   /* This is the block that actually writes the output and increments the
-   * read from the input fifo and increments the state
-   */
-   always @(posedge clk) begin
-      if (reset) begin
-         pro_state        <= PRO_WAIT;
-         out_wr           <= 0;
-         out_data         <= 0;
-         out_ctrl         <= 1;
-      end
-      else begin
-         pro_state       <= pro_state_nxt;
-         out_wr          <= out_wr_nxt;
-         out_data        <= out_data_nxt;
-         out_ctrl        <= out_ctrl_nxt;
-      end
-   end
-
 
 endmodule // myprocessor
